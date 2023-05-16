9396d34780533645fc302d39c36c90ab87585904
GreenNovaStar <brianyip5@gmail.com>
2023-05-15 17:58:12 -0700

    Initial Commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  output: 'export',
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+};
